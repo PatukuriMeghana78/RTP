97f37270fc956519ff33fc3939e44fbda859f61e
PatukuriMeghana78 <patukurimeghana@gmail.com>
2025-05-15 12:38:12 +0530

    web worker for off-thread image processing

---
+++ worker.js
@@ -0,0 +1,48 @@
+// LMS Color Transformations
+function rgbToLms(r, g, b) {
+  return [
+    0.3904725 * r + 0.54990437 * g + 0.00890159 * b,
+    0.07092586 * r + 0.96310739 * g + 0.00135709 * b,
+    0.02314268 * r + 0.12801221 * g + 0.93624394 * b
+  ];
+}
+
+function lmsToRgb(l, m, s) {
+  return [
+    2.85831110 * l - 1.62870796 * m - 0.02482469 * s,
+    -0.21018126 * l + 1.15820096 * m + 0.00032428 * s,
+    -0.04181125 * l - 0.11817878 * m + 1.06871126 * s
+  ];
+}
+
+function simulateCVD(imageData, cvdType) {
+  const data = imageData.data;
+  
+  for (let i = 0; i < data.length; i += 4) {
+    let r = data[i], g = data[i+1], b = data[i+2];
+    let [l, m, s] = rgbToLms(r, g, b);
+
+    // Apply CVD simulation
+    switch(cvdType) {
+      case 'protanopia': l = 0; break;
+      case 'deuteranopia': m = 0; break;
+      case 'tritanopia': s = 0; break;
+    }
+
+    [r, g, b] = lmsToRgb(l, m, s);
+    
+    // Clamp values to 0-255
+    data[i] = Math.max(0, Math.min(255, r));
+    data[i+1] = Math.max(0, Math.min(255, g));
+    data[i+2] = Math.max(0, Math.min(255, b));
+  }
+  
+  return imageData;
+}
+
+// Worker Message Handler
+self.onmessage = function(e) {
+  const { imageData, cvdType } = e.data;
+  const result = simulateCVD(imageData, cvdType);
+  self.postMessage(result, [result.data.buffer]);
+};
